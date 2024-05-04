4605543c13252af5013f060991467c26f7a9d3b8
Marcel <122309581+Dadziek@users.noreply.github.com>
2024-05-04 20:45:16 +0200

    Time: 93 ms (72.49%), Space: 14.1 MB (32.13%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -1,12 +1,3 @@
 class Solution(object):
     def singleNumber(self, nums):
-        for i in range(0, len(nums)):
-            pair = False
-
-            for j in range(0, len(nums)):
-                if nums[i] == nums[j] and i != j:
-                    pair = True
-                    break
-
-            if pair is not True:
-                return nums[i]
+        return 2 * sum(set(nums)) - sum(nums)
